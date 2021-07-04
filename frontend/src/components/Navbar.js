4a1f30a860b1a7d8c898af7c7e36988d995587eb
Seungyeon Yoo <y2s9310@gmail.com>
2021-07-04 16:15:53 +0900

    Fix nav-item route value

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,12 +4,12 @@ export default class Navbar extends Component {
   template() {
     return `
       <ul class="nav-item-list">
-        <li class="nav-item" route="/">HOME</li>
-        <li class="nav-item" route="/life">라이프</li>
-        <li class="nav-item" route="/food">푸드</li>
-        <li class="nav-item" route="/trip">여행</li>
-        <li class="nav-item" route="/culture">컬쳐</li>
-        <li class="nav-item" route="/bookmark">즐겨찾기</li>
+        <li class="nav-item" route="">HOME</li>
+        <li class="nav-item" route="life">라이프</li>
+        <li class="nav-item" route="food">푸드</li>
+        <li class="nav-item" route="trip">여행</li>
+        <li class="nav-item" route="culture">컬쳐</li>
+        <li class="nav-item" route="bookmark">즐겨찾기</li>
       </ul>
     `;
   }
@@ -17,7 +17,8 @@ export default class Navbar extends Component {
   setEvent() {
     const { handleRoute } = this.$props;
     this.addEvent('click', '.nav-item-list', (event) => {
-      handleRoute(event.target);
+      const pathName = event.target.getAttribute('route');
+      handleRoute(pathName);
     });
   }
 }
