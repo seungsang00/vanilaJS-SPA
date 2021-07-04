44eb03b61a67c921186835c7e7f64389c925a903
Seungyeon Yoo <y2s9310@gmail.com>
2021-07-04 10:41:12 +0900

    Add data JSON files

---
+++ backend/data/index.js
@@ -0,0 +1,13 @@
+const rankingData = require('./rankingData.json');
+const lifeData = require('./lifeData.json');
+const foodData = require('./foodData.json');
+const tripData = require('./tripData.json');
+const cultureData = require('./cultureData.json');
+
+module.exports = {
+  top: rankingData,
+  life: lifeData,
+  food: foodData,
+  trip: tripData,
+  culture: cultureData,
+};
