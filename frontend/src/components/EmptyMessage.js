a1082592452553a5c11c0cb6aff889d23aed97ff
Seungyeon Yoo <y2s9310@gmail.com>
2021-07-04 10:48:42 +0900

    Add EmptyMessage component

---
+++ frontend/src/components/EmptyMessage.js
@@ -0,0 +1,36 @@
+import Component from '../core/Component';
+
+export default class EmptyMessage extends Component {
+  setup() {
+    const { visible = false, message } = this.$props;
+    this.$state = {
+      visible,
+      message,
+    };
+  }
+
+  template() {
+    return `
+      <article class="empty-msg">
+        <h2>${this.$state.message}</h2>
+      </article>
+    `;
+  }
+
+  get Visible() {
+    return this.$state.visible;
+  }
+
+  set Visible(value) {
+    this.$state.visible = value;
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+    const { visible } = this.$state;
+    const display = visible === true ? 'block' : 'none';
+    this.$emptyMessage.style.display = display;
+  }
+}
