3e25ae1c47a2baa2ec31a6cd1695b2057c777396
Seungyeon Yoo <y2s9310@gmail.com>
2021-07-04 16:20:15 +0900

    Error: find ContentCard error

--- frontend/src/components/ContentCard.js
+++ frontend/src/components/ContentCard.js
@@ -3,16 +3,16 @@ import Component from '../core/Component';
 export default class ContentCard extends Component {
   setup() {
     this.$state = {
-      ...this.$props.data,
+      data: this.$props.data,
     };
   }
   template() {
     const { type } = this.$props;
 
     if (type === 'basic') {
-      const { idx, title, imageUrl, mediaName, url, summaryContent } = this.$state;
+      const { rank, idx, title, imageUrl, mediaName, url, summaryContent } = this.$state.data;
       return `
-        <div class="content-item content-item-${idx}" data-url=${url} data-index=${index}>
+        <div class="content-item content-item-${idx}" data-url=${url}  data-index=${rank}>
           <div class="content-thumb" data-src=${imageUrl} style="background-image: url(${imageUrl});">
             <button class="content-bookmark-btn" data-content="bookmark-btn-${idx}">☆</button>
           </div>
@@ -22,7 +22,7 @@ export default class ContentCard extends Component {
         </div>
       `;
     } else if (type === 'rank') {
-      const { rank, idx, title, mediaName, url } = this.$state;
+      const { rank, idx, title, mediaName, url } = this.$state.data;
       return `
         <div class="content-item ranking-content content-item-${idx}" data-url=${url}>
           <div class="top12 content-rank">${rank}</div>
@@ -36,14 +36,19 @@ export default class ContentCard extends Component {
   }
   setEvent() {
     const { handleCardClick, handleBookmark } = this.$props;
+    const { idx } = this.$state.data;
+    const data = { ...this.$state.data };
 
-    this.addEvent('click', '.content-item', () => {
-      handleCardClick(this.$state.url);
-    });
-
-    this.addEvent('click', '.content-bookmark-btn', (event) => {
-      event.stopPropagation();
-      handleBookmark(this.$state);
-    });
+    // FIXME: 이벤트 처리가 안된다...
+    const $bookmarkBtn = document.querySelector('.content-bookmark-btn');
+    $bookmarkBtn &&
+      $bookmarkBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        console.log(JSON.stringify(data));
+        console.log('click');
+      });
+  }
+  render() {
+    console.log(this.$target.innerHTML);
   }
 }
